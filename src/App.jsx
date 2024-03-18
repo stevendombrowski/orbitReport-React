f8dead6dddf5a82e129e581d0e79fcdff9b0cfbc
Steven <dombrowskis153@yahoo.com>
2024-03-18 01:26:44 -0400

    task 1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner"
+import useState from "react";
+import satData from "./components/satData";
 import "./App.css"
 import "./components/styling.css"
 
 function App() {
   return (
     <div>
+      <Banner/>
       <Buttons />
       <Table />
     </div>
